2df31742650e1dc8d7e51a10fb348adbd562d609
Ritwick <d.ritwick24@gmail.com>
2020-06-14 12:24:03 +0530

    update readme

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'Firebase';
+import { HomePage } from '../pages/home/home';
+
+const config = {
+  apiKey: "AIzaSyAqno4lZxgc3bBAn7WDO5E0t4OB6Mz21GM",
+  authDomain: "blankfire-88600.firebaseapp.com",
+  databaseURL: "https://blankfire-88600.firebaseio.com",
+  projectId: "blankfire-88600",
+  storageBucket: "blankfire-88600.appspot.com",
+};
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = HomePage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      splashScreen.hide();
+    });
+    firebase.initializeApp(config);
+  }
+}
